e9d1b9793ace8d1cd2264f259ea606e4713b3c78
Jamie Pilgrim <jamieleepilgrim@gmail.com>
2015-02-13 15:31:52 -0600

    added partial and routing

---
+++ js/controllers.js
@@ -0,0 +1,37 @@
+var resumeControllers = angular.module('resumeControllers', []);
+
+resumeControllers.controller('ListController', function MyController($scope) {
+
+	$scope.resumes = [ {
+		'name' : "Jamie Pilgrim",
+		'title' : "Web Developer"
+	},
+	{
+		'name' : "Pilgrim Jamie",
+		'title' : "Web Designer"
+	},
+		{
+		'name' : "John Doe",
+		'title' : "Master Chef"
+	},
+		{
+		'name' : "Prince",
+		'title' : "Singer"
+	},
+		{
+		'name' : "Pluto",
+		'title' : "Star"
+	}];
+	$scope.resumeOrder ='name';
+})
+
+
+// Use something like this to get from a json file: 
+
+// var myApp = angular.module('myApp', []);
+
+// myApp.controller('MyController', ['$scope', '$http', function MyController($scope) {
+// 	$http.get('js/data.json').success(function(data) {
+// 		$scope.resumes = data;
+// 	});
+// }]);
